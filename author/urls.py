b0bc47c9ea7c547e5307a75c7adf53c47210aded
Dmytro Stehantsev <stegan296@gmail.com>
2025-04-25 20:03:08 +0300

    change url prefix to "manage"

--- author/urls.py
+++ author/urls.py
@@ -7,8 +7,8 @@ from author.views import AuthorViewSet
 app_name = "author"
 
 router = routers.DefaultRouter()
-router.register("authors", AuthorViewSet)
+router.register("manage", AuthorViewSet, basename="manage")
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
 ]
